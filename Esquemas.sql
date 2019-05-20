46f35d5e98d36aa237dce0cf18b161f02d8be9a7
William Fabian Gonzalez Cobo <wifagoco@gmail.com>
2019-05-20 11:13:07 -0500

    Se versionan Scripts de Base de datos.

---
+++ Esquemas.sql
@@ -0,0 +1,20 @@
+USE [GESTION_HUMANA_HITSS]
+GO
+/****** Object:  Schema [AUDITORIA]    Script Date: 05/20/2019 10:37:52 ******/
+CREATE SCHEMA [AUDITORIA] AUTHORIZATION [dbo]
+GO
+/****** Object:  Schema [AUDITORIAS]    Script Date: 05/20/2019 10:37:52 ******/
+CREATE SCHEMA [AUDITORIAS] AUTHORIZATION [dbo]
+GO
+/****** Object:  Schema [CONFIGURACIONES]    Script Date: 05/20/2019 10:37:52 ******/
+CREATE SCHEMA [CONFIGURACIONES] AUTHORIZATION [dbo]
+GO
+/****** Object:  Schema [REFERENCIAS]    Script Date: 05/20/2019 10:37:52 ******/
+CREATE SCHEMA [REFERENCIAS] AUTHORIZATION [dbo]
+GO
+/****** Object:  Schema [REQUISICION]    Script Date: 05/20/2019 10:37:52 ******/
+CREATE SCHEMA [REQUISICION] AUTHORIZATION [dbo]
+GO
+/****** Object:  Schema [RETIROS]    Script Date: 05/20/2019 10:37:52 ******/
+CREATE SCHEMA [RETIROS] AUTHORIZATION [dbo]
+GO
